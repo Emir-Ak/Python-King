9fa73ef3aa877e03afcc48ea25ebc639346bdff5
Anion_Z <emir_akbashev@mail.ru>
2018-02-07 16:33:38 +0300

    Everything restarted...

---
+++ Assets/Old/Scripts/SpriteChange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SpriteChange : MonoBehaviour
+{
+    private float timeBeforeNextSprite = 0f;
+    private float delay = 0f;
+
+    Image image;
+
+    [Header("Sprites that will change through animation")]
+
+    public List<Sprite> spriteList;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        StartCoroutine(SpriteChangeCoroutine());
+    }
+    IEnumerator SpriteChangeCoroutine()
+    {
+            yield return new WaitForSeconds(delay);
+
+            yield return new WaitForSeconds(timeBeforeNextSprite);
+
+            foreach (Sprite i in spriteList)
+            {
+                image.sprite = i;
+                image.color = new Color(255,255,255,0);
+                if (i != spriteList[spriteList.Count - 1])
+                {
+                    yield return new WaitForSeconds(timeBeforeNextSprite);
+                }
+
+            }
+
+
+    }
+}
